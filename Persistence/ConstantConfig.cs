cdfa68831778b4c8f4fe698336e0129eab38092d
Jan Sebastian <jnsbstn391@gmail.com>
2026-04-12 03:58:58 +0700

    fix (BAU-PROTO)
    - fix constant gettingappSettings for every env

--- Persistence/ConstantConfig.cs
+++ Persistence/ConstantConfig.cs
@@ -5,5 +5,12 @@
         public const string DbConfig = "DefaultConnection";
         public const string ViewRoot = "index.html";
         public const string Sec = "bahl";
+        //public const string AppConfig = $"appsettings.{ConstantConfig.env ?? ""}.json";
+
+        public static string GetAppConfig()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            return $"appsettings.{env ?? ""}.json";
+        }
     }
 }
