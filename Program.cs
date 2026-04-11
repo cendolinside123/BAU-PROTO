edb9a57912b952cf903a8309dc196ae0e23cd780
Jan Sebastian <jnsbstn391@gmail.com>
2026-04-12 00:39:06 +0700

    feat (BAU-PROTO)
    - update Jwt handle / config

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = builder.Configuration.GetSection("bahl").Value;
+var key = builder.Configuration.GetSection(ConstantConfig.Sec).Value;
 
 builder.Services.AddAuthentication(options =>
 {
