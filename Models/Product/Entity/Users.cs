0a5a4d9a5c06cc486c36c6cdc3fdff9980d0ee28
Jan Sebastian <jnsbstn391@gmail.com>
2026-04-12 01:09:59 +0700

    fix:
    fix (BAU-PROTO)
    
    - fix wrong model name

---
+++ Models/Product/Entity/Users.cs
@@ -0,0 +1,10 @@
+﻿public class Users
+{
+    public int Id { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? LoginAt { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiresAt { get; set; }
+}
